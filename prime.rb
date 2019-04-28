ba86ed851b560c1eefc6db855cbf0b55f88ae9ad
Maasa Kono <konomaasa@gmail.com>
2019-04-28 20:20:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(num)
   (2..(num - 1)).each do |x|
-    if num % x != 0
-      return true
-    else num % x == 0
+    return true if num % x != 0
+  end
+    num % x == 0
       false
     end
-  end
 end
