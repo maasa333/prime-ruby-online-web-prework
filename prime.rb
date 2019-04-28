a589aefbcb0c9c51f73d0beb8930bd5ec49e9413
Maasa Kono <konomaasa@gmail.com>
2019-04-28 20:36:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,9 @@
 def prime?(num)
   num = num.ab
   if num > 2 do
-  (2..(num - 1)).each do |n|
+    (2..(num - 1)).each do |n|
     return false if num % n == 0
+    end
   else
     return true
-  end
 end
