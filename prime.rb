52e3d3d5a7159c87496b2d2addd62c77ba27c9c6
Maasa Kono <konomaasa@gmail.com>
2019-04-28 20:53:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  num = num.abs
+  if num > 2
+    (2..(num - 1)).each do |n|
+      return false if num % n == 0
+    end
+    return true
+  elsif num < 2
+    return false
+  else
+    return true
+  end
+end
